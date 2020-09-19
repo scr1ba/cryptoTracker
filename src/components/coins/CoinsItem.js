2e4e1f5c223fc65612aa93f7f3ad56cc46de2b7b
Kevin <3278943+spongevek@users.noreply.github.com>
2020-09-20 00:43:00 +0100

    Added arrows depending on last hour price and some styles to the list

--- src/components/coins/CoinsItem.js
+++ src/components/coins/CoinsItem.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, Platform} from 'react-native';
+import colors from '../../res/colors';
 
 const CoinsItem = ({item}) => {
+  let getImgArrow = () => {
+    if (item.percent_change_1h > 0) {
+      return require('cryptoTracker/src/assets/arrow_up.png');
+    } else {
+      return require('cryptoTracker/src/assets/arrow_down.png');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.row}>
         <Text style={styles.symbolText}>{item.symbol}</Text>
         <Text style={styles.nameText}>{item.name}</Text>
+        <Text style={styles.priceText}>{`$${item.price_usd}`}</Text>
       </View>
       <View style={styles.row}>
-        <Text style={styles.change}>{'' + item.percent_change_1h}</Text>
+        <Text style={styles.percentChange}>{'' + item.percent_change_1h}</Text>
+        <Image source={getImgArrow()} style={styles.imageIcon} />
       </View>
     </View>
   );
@@ -18,19 +29,38 @@ const CoinsItem = ({item}) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
     padding: 16,
+    borderBottomColor: colors.zircon,
+    borderBottomWidth: 1,
+    marginLeft: (Platform.OS = 'ios' ? 16 : 0),
   },
   row: {
     flexDirection: 'row',
   },
+  priceText: {
+    color: colors.white,
+    fontSize: 14,
+  },
   symbolText: {
-    color: '#fff',
+    color: colors.white,
     fontWeight: 'bold',
     fontSize: 16,
+    marginRight: 12,
   },
   nameText: {
-    color: '#fff',
+    color: colors.white,
     fontSize: 14,
+    marginRight: 16,
+  },
+  percentChange: {
+    color: colors.white,
+    fontSize: 12,
+    marginRight: 8,
+  },
+  imageIcon: {
+    width: 22,
+    height: 22,
   },
 });
 
